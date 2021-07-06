669961c3f2399bd44f0f8d7e4ecce539904db5ff
Hakim Ghods <ghods.hakim@hotmail.com>
2021-07-06 04:33:11 +0430

    Add project files.

---
+++ src/YieldReturnDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace YieldReturnDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var count = 10;
+            var powers = PowerBy2Yield(count);
+            foreach (var item in powers)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static IList<double> PowerBy2List(int count)
+        {
+            var result = new List<double>();
+            for (int i = 1; i < count + 1; i++)
+            {
+                result.Add(Math.Pow(2, i));
+            }
+
+            return result;
+
+        }
+
+        public static IEnumerable<double> PowerBy2Yield(int count)
+        {
+            var result = new List<double>();
+            for (int i = 1; i < count + 1; i++)
+            {
+                yield return Math.Pow(2, i);
+            }        
+
+        }
+    }
+}
